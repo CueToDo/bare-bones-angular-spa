a2fc4a15b4766b9d3dbcb78593fbe360d9054cba
Alex Cooper <alex@free.vote>
2024-06-08 09:54:23 +0100

    server.js => server.ts

--- server.ts
+++ server.ts
@@ -7,10 +7,11 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/bare-bones-angular-spa/browser'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/bare-bones-angular-spa/browser/index.html'));
+app.get('/*', function (_req: any, res: { sendFile: (arg0: any) => void }) {
+  res.sendFile(
+    path.join(__dirname + '/dist/bare-bones-angular-spa/browser/index.html')
+  );
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env['PORT'] || 8080);
